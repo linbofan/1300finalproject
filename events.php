4e4829a06c33bb05ced4e06073d19d2640e2347d
linbofan <lf255@cornell.edu>
2016-12-07 15:59:14 -0500

    Add files via upload

---
+++ events.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,maximum-scale=1.0,initial-scale=1.0">
+    <meta name="description" content="Ithaca Apple Festival">
+    <meta name="renderer" content="webkit">
+    <title>Ithaca Apple Festival</title>
+    <link rel="stylesheet" type="text/css" href="styles/events.css">
+    <link rel="stylesheet" type = "text/css" href="styles/site.css">
+</head>
+<body>
+    
+    
+   <?php include 'includes/header.htm';?>
+    
+    
+<div class = "content">
+<h1>Calendar of Events!</h1>
+
+<div class="month">      
+  <ul>
+    <li class="prev">❮</li>
+    <li class="next">❯</li>
+    <li>September<br>
+      <span>2016</span>
+    </li>
+  </ul>
+</div>
+
+<ul class="weekdays">
+  <li>Mo</li>
+  <li>Tu</li>
+  <li>We</li>
+  <li>Th</li>
+  <li>Fr</li>
+  <li>Sa</li>
+  <li>Su</li>
+</ul>
+
+<ul class="days"> 
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li><span class="active">30</span></li>
+  <li><span class="active">1</span></li>
+  <li><span class="active">2</span></li>
+  <li>3</li>
+  <li>4</li>
+  <li>5</li>
+  <li>6</li>
+  <li>7</li>
+  <li>8</li>
+  <li>9</li>
+  <li>10</li>
+  <li>11</li>
+  <li>12</li>
+  <li>13</li>
+  <li>14</li>
+  <li>15</li>
+  <li>16</li>
+  <li>17</li>
+  <li>18</li>
+  <li>19</li>
+  <li>20</li>
+  <li>21</li>
+  <li>22</li>
+  <li>23</li>
+  <li>24</li>
+  <li>25</li>
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li>30</li>
+  <li>31</li>
+</ul>
+    <h1>Schedule of Events</h1>
+        <p>Event Times:<br>
+    Friday 12pm-6pm<br>
+    Saturday and Sunday 10am-6pm</p>
+<p>2016 MAIN STAGE SCHEDULE:
+BERNIE MILTON PAVILION </p> 
+ 
+<ul>
+    <li>SATURDAY 10-10:45AM Punk Hands</li>
+    <li>SATURDAY 11-11:45AM Danza Romani</li>
+    <li>SATURDAY 12-12:45PM Vitamin L</li>
+    <li>SATURDAY 1:00-1:45PM Riley Owens</li>
+    <li>SATURDAY 1:45-2pm  GIVEAWAYS</li>
+    <li>SATURDAY 2:00-2:45PM Sunny Weather</li>
+    <li>SATURDAY 3:00-3:45PM Eems</li>
+    <li>SATURDAY 4:00-4:45PM Alter</li>
+    <li>SATURDAY 5:00-6:00pm Wailin' Brass Band</li></ul>
+    
+    
+<ul>
+    <li>SUNDAY 10:00-10:45AM IC Voicestream, Ithacapella, Premium Blend</li>
+    <li>SUNDAY 11:00-11:45AM Sixteen Feathers</li>
+    <li>SUNDAY 12:00-12:45PM June w/ Chandani</li>
+    <li>SUNDAY 1:00-1:45PM Mary Lorson</li>
+    <li>SUNDAY 2:00-3:00PM Dapper Dan</li>
+    <li>SUNDAY 3:15-4:15PM Radio London</li>
+    <li>SUNDAY 4:30-5:00PM Mirage Belly Dancers</li>
+    <li>SUNDAY 5:15-6:15PM NEO Project</li>
+    </ul>
+    
+<p>WEST STATE STREET</p>
+<ul>    
+    <li>FRIDAY 12:30 and 4:45pm Hilby The Skinny German Juggle Boy </li></ul>
+ 
+<ul>
+    <li>SATURDAY 12pm- Zajal the Sugarplum Fairy</li>
+    <li>SATURDAY 1pm- The Next Jenneration</li> 
+    <li>SATURDAY 3pm- The Strong Woman Show</li>
+    <li>SATURDAY 4:30- IC Circus</li></ul>
+ 
+
+<ul>
+    <li>SUNDAY 12:30pm Matan Presberg - Juggler</li>
+    <li>SUNDAY 3pm- Hula Hut Polynesian Dance</li></ul>
+    </div>
+<footer>
+      September,2016
+</footer>
+    <script src="js/jquery-3.1.1.min.js"></script>
+</body>
+</html>
---
+++ gallery.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,maximum-scale=1.0,initial-scale=1.0">
+    <meta name="description" content="Ithaca Apple Festival">
+    <meta name="renderer" content="webkit">
+    <title>Ithaca Apple Festival</title>
+    <link rel="stylesheet" type="text/css" href="styles/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/grid.css">
+    <link rel="stylesheet" type="text/css" href="styles/site.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+  </head>
+
+  <body>
+
+
+    <?php include 'includes/header.htm';?>
+
+     <div class="container res">
+
+
+      <div class="grid-g gallery-box">
+    <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0833.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0833.jpg " alt="fruits"/>
+	    <span>Fruits</span>
+	  </a>
+	</div>
+    <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0875.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0875.jpg" alt="cakes"/>
+	    <span>Cakes</span>
+	  </a>
+	</div>
+    <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0866.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0866.jpg " alt="cider"/>
+	    <span>Apple cider</span>
+	  </a>
+	</div>
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0835.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0835.jpg " alt="accessories"/>
+	    <span>Accessories</span>
+	  </a>
+	</div>
+    <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0846.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0846.jpg " alt="decorates"/>
+	    <span>Decorates</span>
+	  </a>
+	</div>
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0841.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0841.jpg " alt="arts"/>
+	    <span>Arts</span>
+	  </a>
+	</div>
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0849.jpg.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0849.jpg" alt="booths"/>
+	    <span>Booths</span>
+	  </a>
+	</div>
+    <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0857.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0857.jpg " alt="music"/>
+	    <span>Music</span>
+	  </a>
+	</div>
+     <!-- photo from: Yi Feng-->
+	<div class="grid-u-1-3 grid-xx-1">
+	  <a href="images/gallery/IMG_0853.jpg" target="_blank">
+	    <img src="images/gallery/thumb_0853.jpg " alt="fun"/>
+	    <span>More Fun</span>
+	  </a>
+	</div>
+
+      </div>
+
+    </div>
+
+
+    <footer>
+      September,2016
+    </footer>
+
+    <script src="js/jquery-3.1.1.min.js"></script>
+
+</body>
+</html>
